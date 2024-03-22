f9487e81c8196263df003e61615e43d68bf5ee2b
ismet efe balık <efebalikofc@hotmail.com>
2024-03-22 21:20:24 +0300

    first

---
+++ ex01/main.cpp
@@ -0,0 +1,11 @@
+#include "iter.hpp"
+
+void print(int const &i) {
+    std::cout << i << std::endl;
+}
+
+int main( void ) {
+    int tab[] = { 0, 1, 2, 3, 4 };
+    ::iter(tab, 5, print);
+    return 0;
+}
---
+++ ex02/Array.hpp
@@ -0,0 +1,48 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+template <class T>
+class   Array
+{
+    private:
+        T *Arr;
+        unsigned int size;
+    public:
+        Array()
+        {
+            this->Arr = new T[0];
+        }
+        ~Array()
+        {
+            delete this->Arr;
+        }
+        Array(unsigned int n)
+        {
+            this->Arr = new T[n];
+            this->size = n;
+        }
+        Array(const Array &copy, unsigned int n)
+        {
+            this->Arr = new T[n];
+            for (unsigned int i = 0; i < n; i++)
+                this->Arr[i] = copy.Arr[i];
+        }
+        Array& operator= (const Array &copy)
+        {
+            this->Arr = new T[this->size];
+            for (int i = 0; i < this->size ; i++)
+                this->Arr[i] = copy.Arr[i];
+            return *this;
+        }
+        T& operator[] (unsigned int index)
+        {
+            if(index >= this->size)
+                throw std::out_of_range("Index out of range");
+            else
+                return this->Arr[index];
+        }
+};
+
+#endif
